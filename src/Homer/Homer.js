256dfbd9c053e3e18df88d5996c18c149ccad026
Ateriss <alexandralinares2729@gmail.com>
2023-04-26 20:27:16 -0500

    .

--- src/Homer/Homer.js
+++ src/Homer/Homer.js
@@ -21,8 +21,8 @@ function Homer (){
                     Sigue a Alexandra en: 
                 </p>
                 <div className="homer__rrss">
-                    <a className="rrss" href="https://github.com/Ateriss"  target="_blank">  <GitHub color="#424F46" size={32} /> </a>
-                    <a className="rrss" href="https://www.linkedin.com/in/alexandra-linares-vi%C3%B1a-67b027125/" target="_blank" >  <Linkedin color="#424F46" size={32} /> </a>
+                    <a className="rrss" href="https://github.com/Ateriss"  target="_blank" rel="noopener noreferrer">  <GitHub color="#424F46" size={32} /> </a>
+                    <a className="rrss" href="https://www.linkedin.com/in/alexandra-linares-vi%C3%B1a-67b027125/" target="_blank" rel="noopener noreferrer" >  <Linkedin color="#424F46" size={32} /> </a>
                 </div>
                 <div className="links">
                     <a className="link" target="_blank" rel="noopener noreferrer"  href="https://ateriss.github.io/Portafolio-Personal/" ><img src={Ateris} className="ateris" alt="Logo Ateris" /> <p>Haz click aquí para ver su portafolio</p><img src={Ateris} alt="Logo Ateris" className="ateris" /> </a>
